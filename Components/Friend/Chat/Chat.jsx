a6f605ee1c6a3438c6e137742e7192e81a8e3048
alfredrodriguez98 <alfredrodriguez1998@outlook.com>
2022-11-02 21:05:05 +0530

    Chat module css

--- Components/Friend/Chat/Chat.jsx
+++ Components/Friend/Chat/Chat.jsx
@@ -30,9 +30,96 @@ const Chat = ({
     setChatData(router.query);
   }, [router.isReady]);
 
-  console.log(chatData.address, chatData.name);
+  // console.log(chatData.address, chatData.name);
 
-  return <div>Chat</div>;
+  return (
+    <div className={Style.Chat}>
+      {currentUserName && currentUserAddress ? (
+        <div className={Style.Chat_user_info}>
+          <Image src={images.accountName} alt="image" width={70} height={70} />
+          <div className={Style.Chat_user_info_box}>
+            <h4>{currentUserName}</h4>
+            <p className={Style.show}>{currentUserAddress}</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <div className={Style.chat_box_box}>
+        <div className={Style.Chat_box}>
+          <div className={Style.Chat_box_left}>
+            {friendMsg.map((el, i) => (
+              <div>
+                {el.sender == chatData.address ? (
+                  <div className={Style.Chat_box_left_title}>
+                    <Image
+                      src={images.accountName}
+                      alt="image"
+                      width={50}
+                      height={50}
+                    />
+                    <span>
+                      {chatData.name} {""}
+                      <small>Time: {convertTime(el.timestamp)}</small>
+                    </span>
+                  </div>
+                ) : (
+                  <div className={Style.Chat_box_left_title}>
+                    <Image
+                      src={images.accountName}
+                      alt="image"
+                      width={50}
+                      height={50}
+                    />
+                    <span>
+                      {userName} {""}
+                      <small>Time: {convertTime(el.timestamp)}</small>
+                    </span>
+                  </div>
+                )}
+                <p key={i + 1}>
+                  {el.msg}
+                  {""}
+                  {""}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {currentUserName && currentUserAddress ? (
+          <div className={Stryle.Chat_box_send}>
+            <div className={Style.Chat_box_send_img}>
+              <Image src={images.smile} alt="smile" width={50} height={50} />
+              <input
+                type="text"
+                placeholder="type your message"
+                onChange={(e) => setMessage(e.target.value)}
+              />
+
+              <Image src={images.file} alt="file" width={50} height={50} />
+              {loading == true ? (
+                <Loader />
+              ) : (
+                <Image
+                  src={images.send}
+                  alt="file"
+                  width={50}
+                  height={50}
+                  onClick={() =>
+                    functionName({ msg: message, address: chatData.address })
+                  }
+                />
+              )}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
